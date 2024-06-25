ab9db6f81f19abf20f2dcce56fa560f47b9702c0
Carson Wu <topics.copilot0n@icloud.com>
2024-06-26 01:18:02 +0800

    Create phonenubers.py

---
+++ Tools/phonenubers.py
@@ -0,0 +1,11 @@
+# Copyright © 2024 Carson. All rights reserved.
+
+import phonenumbers
+from phonenumbers import geocoder
+
+phone_number1 = phonenumbers.parse("PHONE_NUMBER_1")
+phone_number2 = phonenumbers.parse("PHONE_NUMBER_2")
+
+print("\nPhone Numbers Location\n")
+print(geocoder.description_for_number(phone_number1, "en"))
+print(geocoder.description_for_number(phone_number2, "en"))

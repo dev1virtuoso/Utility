f2cbca4ae16d55d9d38e0bb31c7771054912111c
Carson Wu <topics.copilot0n@icloud.com>
2024-07-12 11:53:16 +0800

    Add files via upload

---
+++ Tools/clone_repositories.sh
@@ -0,0 +1,26 @@
+# Copyright © 2024 Carson. All rights reserved.
+#!/bin/bash
+
+repositories=(
+  "REPO_LINK_1"
+  "REPO_LINK_2"
+  "REPO_LINK_3"
+  "REPO_LINK_4"
+  "REPO_LINK_5"
+  "REPO_LINK_6"
+  "REPO_LINK_7"
+  "REPO_LINK_8"
+  "REPO_LINK_9"
+  "REPO_LINK_10"
+)
+
+target_folder="my_repositories"
+
+mkdir -p "$target_folder"
+
+cd "$target_folder"
+
+for repo in "${repositories[@]}"
+do
+  git clone "$repo"
+done

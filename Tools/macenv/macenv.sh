f1c60347746dd07b8af279b423e32f84269d9185
Carson Wu <topics.copilot0n@icloud.com>
2024-07-14 16:24:55 +0800

    Update macenv.sh

--- Tools/macenv/macenv.sh
+++ Tools/macenv/macenv.sh
@@ -18,8 +18,31 @@ brew install mongodb
 brew install elasticsearch
 brew install awscli
 brew install zsh zsh-completions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-curl -L https://get.rvm.io | bash -s stable --ruby
+brew install git
+brew install gitkraken-cli
+brew install gmp
+brew install gnupg
+brew install sqlite
+brew install postgresql
+brew services start postgresql
+brew install zsh
+brew tap heroku/brew
+brew install heroku
 git config --global user.name “USER_NAME”
 git config --global user.email “EMAIL”
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+curl -L https://get.rvm.io | bash -s stable --ruby
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl "https://raw.githubusercontent.com/flatiron-school/dotfiles/master/ubuntu-gitignore" -o "$HOME/.gitignore"
+curl "https://raw.githubusercontent.com/flatiron-school/dotfiles/master/gitconfig" -o "$HOME/.gitconfig"
+curl -so- https://raw.githubusercontent.com/learn-co-curriculum/flatiron-manual-setup-validator/master/manual-setup-check.sh | bash 2> /dev/null
+nvm install v16.13.2
+nvm install node
+npm install --global yarn
+npm install --global pm2
+gem install pg
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+source ~/.zshrc
 echo "Development environment setup complete."

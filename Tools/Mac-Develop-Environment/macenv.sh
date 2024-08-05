032809cbef9eed0a289c1fe3e48a20cc09796df0
Carson Wu <topics.copilot0n@icloud.com>
2024-08-06 00:44:12 +0800

    Update macenv.sh

--- Tools/Mac-Develop-Environment/macenv.sh
+++ Tools/Mac-Develop-Environment/macenv.sh
@@ -2,6 +2,9 @@
 
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo sh -c 'xcode-select -s /Applications/Xcode.app/Contents/Developer && xcodebuild -runFirstLaunch'
+sudo xcodebuild -license
+xcodebuild -downloadAllPlatforms
 brew install git
 brew install readline
 brew install node
@@ -28,8 +31,12 @@ brew install zsh
 brew tap heroku/brew
 brew install heroku
 brew install wget
-git config --global user.name “USER_NAME”
-git config --global user.email “EMAIL”
+read -p "Enter your Git username: " username
+read -p "Enter your Git email address: " email
+git config --global user.name "$username"
+git config --global user.email "$email"
+echo "Git username set to: $username"
+echo "Git email address set to: $email"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 curl -L https://get.rvm.io | bash -s stable --ruby
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -42,6 +49,7 @@ nvm install node
 npm install --global yarn
 npm install --global pm2
 gem install pg
+gem install cocoapods
 echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
 source ~/.zshrc

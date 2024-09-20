583cbb3ef1828199e66e6fed706314ee1985a7a1
Carson Wu <carson.developer1125@gmail.com>
2024-09-21 00:23:09 +0800

    updated mac dev env

---
+++ Tools/Mac-Develop-Environment/macenv-doctor.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Print Xcode path
+echo "Xcode Path:"
+xcode-select --print-path
+echo
+
+# List Homebrew packages
+echo "Homebrew Packages:"
+brew list
+echo
+
+# Git user configuration
+echo "Git User Configuration:"
+echo "Username:"
+git config --global user.name
+echo "Email:"
+git config --global user.email
+echo
+
+# List contents of a directory
+echo "Contents of Cloned Repository:"
+ls /path/to/cloned/repository
+echo
+
+# List RVM managed Ruby versions
+echo "RVM Ruby Versions:"
+rvm list
+echo
+
+# Node.js and related tools versions
+echo "Node.js and Related Tools:"
+echo "nvm:"
+nvm --version
+echo "Node.js:"
+node --version
+echo "Yarn:"
+yarn --version
+echo "PM2:"
+pm2 --version
+echo
+
+# Ruby environment
+echo "Ruby Environment:"
+echo "Ruby version:"
+ruby -v
+echo "Installed Gems:"
+gem list
+echo
+
+# PHP and Composer versions
+echo "PHP and Composer Versions:"
+echo "PHP:"
+php --version
+echo "Composer:"
+composer --version
+echo
+
+# Pyenv help
+echo "Pyenv Help:"
+pyenv --help
+echo
+
+# Rust compiler version
+echo "Rust Compiler Version:"
+rustc --version
--- Tools/Mac-Develop-Environment/macenv.sh
+++ Tools/Mac-Develop-Environment/macenv.sh
@@ -1,38 +1,73 @@
 #!/bin/bash
 
+# Install Xcode Command Line Tools
 xcode-select --install
+
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Rosetta for Apple silicon
 softwareupdate --install-rosetta
+
+# Switch Xcode path and accept the license
 sudo sh -c 'xcode-select -s /Applications/Xcode.app/Contents/Developer && xcodebuild -runFirstLaunch'
 sudo xcodebuild -license
 xcodebuild -downloadAllPlatforms
+
+# Update Homebrew and install dependencies
+brew update
 xargs brew install < brew-requirements.txt
 brew install --cask visual-studio-code
-brew install --cask adoptopenjdk
-brew install --cask android-studio
-brew install --cask docker
-brew services start postgresql
-brew tap heroku/brew
-brew install lporg
+# Add more brew installations as needed
+
+# Configure Git
 read -p "Enter your Git username: " username
 read -p "Enter your Git email address: " email
 git config --global user.name "$username"
 git config --global user.email "$email"
 echo "Git username set to: $username"
 echo "Git email address set to: $email"
+
+# Clone necessary Git repositories
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Install RVM and NVM
 curl -L https://get.rvm.io | bash -s stable --ruby
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Download specific Gitignore and Gitconfig files
 curl "https://raw.githubusercontent.com/flatiron-school/dotfiles/master/ubuntu-gitignore" -o "$HOME/.gitignore"
 curl "https://raw.githubusercontent.com/flatiron-school/dotfiles/master/gitconfig" -o "$HOME/.gitconfig"
+
+# Run setup validation script
 curl -so- https://raw.githubusercontent.com/learn-co-curriculum/flatiron-manual-setup-validator/master/manual-setup-check.sh | bash 2> /dev/null
+
+# Other tool installations
+curl https://pyenv.run | bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Setup Node.js and related tools
 nvm install v16.13.2
 nvm install node
 npm install --global yarn
 npm install --global pm2
+
+# Install necessary Ruby gems
 gem install pg
 gem install cocoapods
+gem update --system
+
+# Setup Composer for PHP
+php composer-setup.php --install-dir=bin --filename=composer
+
+# Update shell configuration
 echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.zshrc
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo 'export PATH="/opt/homebrew/opt/php@8.0/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/opt/homebrew/opt/php@8.0/sbin:$PATH"' >> ~/.zshrc
+export LDFLAGS="-L/opt/homebrew/opt/php@8.0/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/php@8.0/include"
+echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.bash_profile
+
+# Source the updated shell configuration
 source ~/.zshrc

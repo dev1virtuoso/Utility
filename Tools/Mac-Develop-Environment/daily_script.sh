1a0b3c62a1137711ae9cca71ecd954475b2885b9
dev1virtuoso <carson.developer1125@gmail.com>
2025-05-03 14:42:25 +0800

    updated MDE

--- Tools/Mac-Develop-Environment/daily_script.sh
+++ Tools/Mac-Develop-Environment/daily_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Run smartctl command and save output to disk3s3.txt
 smartctl -a disk3s3
 
 # Save pip freeze output to a.txt
@@ -13,3 +12,30 @@ brew upgrade --cask
 
 # Save Launchpad setting
 lporg save
+
+# Function to recursively search for Git repositories in subdirectories
+function update_git_repos {
+    local folder="$1"
+    
+    # Iterate through all items in the folder
+    for item in "$folder"/*; do
+        if [ -d "$item" ]; then
+            # If the item is a directory
+            if [ -d "$item/.git" ]; then
+                echo "Pulling latest changes for $item"
+                # Change to the directory and execute git pull
+                (cd "$item" && git pull)
+                echo ""
+            else
+                # Recursively call the function for subdirectories
+                update_git_repos "$item"
+            fi
+        fi
+    done
+}
+
+# Specify the folder path to start the search
+folder_path="/path/to/directory"
+
+# Call the function with the specified folder path
+update_git_repos "$folder_path"
